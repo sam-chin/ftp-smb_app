05e8db512366fe655b0b342846ece01fe661b935
Sam Chin <1650962165@qq.com>
2026-05-06 19:10:55 +0800

    Cleanup: Remove unused LocalContext import

--- app/src/main/java/com/example/lanmediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/lanmediaplayer/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.ui.PlayerView
