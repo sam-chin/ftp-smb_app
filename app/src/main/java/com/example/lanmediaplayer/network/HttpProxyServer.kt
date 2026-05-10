b173a7d829dc8be29a4c02741d74dfc891cba9ce
Sam Chin <1650962165@qq.com>
2026-05-11 00:19:40 +0800

    new3

--- app/src/main/java/com/example/lanmediaplayer/network/HttpProxyServer.kt
+++ app/src/main/java/com/example/lanmediaplayer/network/HttpProxyServer.kt
@@ -8,8 +8,8 @@ import java.net.NetworkInterface
 import java.net.ServerSocket
 import java.net.Socket
 import java.net.URLDecoder
-import java.nio.charset.Charsets
 import java.util.Collections
+import kotlin.text.Charsets
 
 /**
  * HTTP 代理服务器
