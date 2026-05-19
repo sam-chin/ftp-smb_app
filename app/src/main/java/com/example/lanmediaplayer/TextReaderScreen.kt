3c03a924829db1d7dc14855d2ed5e7797e8d5862
Sam Chin <1650962165@qq.com>
2026-05-20 04:13:46 +0800

    new3

--- app/src/main/java/com/example/lanmediaplayer/TextReaderScreen.kt
+++ app/src/main/java/com/example/lanmediaplayer/TextReaderScreen.kt
@@ -22,8 +22,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lanmedia.player.controller.MediaController
